b9de92e35a8c90ca1f6efce15efd4e0ed6217a17
MatsProg <pawelgixer@gmail.com>
2020-04-11 08:22:08 +0200

    Update

--- test.py
+++ test.py
@@ -7,7 +7,12 @@ class Application(tk.Frame):
         self.master = master
         self.pack()
         self.create_widgets()
+        self.UpdateBar(40, 50, 60, 70)
         self.Progressbar1()
+
+
+
+
 
     def create_widgets(self):
         self.hi_there = tk.Button(self)
@@ -18,7 +23,15 @@ class Application(tk.Frame):
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
         self.quit.pack(side="bottom")
+
+    def UpdateBar(self, P1, P2, P3, P4):
+        self.P1 = P1
+        self.P2 = P2
+        self.P3 = P3
+        self.P4 = P4
+        return self.P1, self.P2, self.P3, self.P4
     def Progressbar1(self):
+
         self.paa1 = Progressbar(root, orient="vertical", length=200, mode="determinate")
         self.paa2 = Progressbar(root, orient="vertical", length=200, mode="determinate")
         self.paa3 = Progressbar(root, orient="vertical", length=200, mode="determinate")
@@ -26,9 +39,13 @@ class Application(tk.Frame):
         self.paaList = [self.paa1, self.paa2, self.paa3, self.paa4]
         for x in self.paaList:
             x.pack()
-            x.start()
-        #self.paa1.pack()
-        #self.paa1.start()
+           # x.start()
+        self.paa1['value'] = self.P1
+        self.paa2['value'] = self.P2
+        self.paa3['value'] = self.P3
+        self.paa4['value'] = self.P4
+
+
 
 
     def say_hi(self):
@@ -37,3 +54,4 @@ class Application(tk.Frame):
 root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
+app.UpdateBar(40,50,60,90)
