c45da1e9baccc75b7e3f720718733a35bbe2eca2
MatsProg <pawelgixer@gmail.com>
2020-04-26 23:08:19 +0200

    up

--- GUI.py
+++ GUI.py
@@ -1,35 +1,37 @@
 import tkinter as tk
 from tkinter.ttk import Progressbar
-from tkinter import ttk
 
 
 class tkinter1():
-    def __init__(self):
-        self.app = tk.Tk()
+	def __init__(self):
+		self.app = tk.Tk()
+
+	def MainWindow(self):
+		self.app.title('Manager')
+
+		self.app.geometry('700x400')
+
+		self.app.image = tk.PhotoImage(file='2.png')
+		self.label = tk.Label(self.app, image=self.app.image, bg='white')
+		self.app.wm_attributes("-transparentcolor", "white")  # TRANSPARENCY
+		self.label.pack()
+		self.app.lift()
+		self.app.mainloop()
+		bar = Progressbar(self.app, length=200, style='black.Horizontal.TProgressbar')
+		bar['value'] = 70
+		bar.grid(column=0, row=0)
 
 
-    def MainWindow(self):
-
-        self.app.title('Manager')
-        self.app.geometry('700x400')
-        #self.app.image = tk.PhotoImage(file='2.png')
-       # self.label = tk.Label(self.app, image=self.app.image, bg='white')
-       # self.app.wm_attributes("-transparentcolor", "white")  # TRANSPARENCY
-        #self.label.pack()
-        self.app.lift()
-        self.app.mainloop()
-    def Bar(self):
-        style = ttk.Style()
-        style.theme_use('default')
-        style.configure("black.Horizontal.TProgressbar", background='black')
-        self.bar = Progressbar(self.app, length=200, style='black.Horizontal.TProgressbar')
-        self.bar['value'] = 70
-        self.bar.grid(column=0, row=0)
+	# self.style = ttk.Style()
+	# self.style.theme_use('default')
+	# self.style.configure("black.Horizontal.TProgressbar", background='black')
 
 
 
 
 
+# bb = tkinter1
+# bb.Bar()
 
 # app.overrideredirect(True) #Borderless
 
--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 import tkinter as tk
 from tkinter.ttk import *
 
+
+
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.create_widgets()
-        self.UpdateBar(40, 50, 60, 70)
-        self.Progressbar1()
 
+        #self.create_widgets()
+        #self.Progressbar1(22)
 
-
-
+    def add(a):
+        # returning sum of a and b
+        return a
 
     def create_widgets(self):
         self.hi_there = tk.Button(self)
@@ -24,26 +27,12 @@ class Application(tk.Frame):
                               command=self.master.destroy)
         self.quit.pack(side="bottom")
 
-    def UpdateBar(self, P1, P2, P3, P4):
-        self.P1 = P1
-        self.P2 = P2
-        self.P3 = P3
-        self.P4 = P4
-        return self.P1, self.P2, self.P3, self.P4
-    def Progressbar1(self):
+    def Progressbar1(self, w):
 
-        self.paa1 = Progressbar(root, orient="vertical", length=200, mode="determinate")
-        self.paa2 = Progressbar(root, orient="vertical", length=200, mode="determinate")
-        self.paa3 = Progressbar(root, orient="vertical", length=200, mode="determinate")
-        self.paa4 = Progressbar(root, orient="vertical", length=200, mode="determinate")
-        self.paaList = [self.paa1, self.paa2, self.paa3, self.paa4]
-        for x in self.paaList:
-            x.pack()
-           # x.start()
-        self.paa1['value'] = self.P1
-        self.paa2['value'] = self.P2
-        self.paa3['value'] = self.P3
-        self.paa4['value'] = self.P4
+        self.paa1 = Progressbar(root, orient="horizontal", length=200, mode="determinate")
+        self.paa1.pack()
+
+        self.paa1['value'] = Application.add(w)
 
 
 
@@ -53,5 +42,7 @@ class Application(tk.Frame):
 
 root = tk.Tk()
 app = Application(master=root)
+#app.Upd(4)
+app.Progressbar1()
 app.mainloop()
-app.UpdateBar(40,50,60,90)
+#
